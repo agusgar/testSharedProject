1106f2d568603644c062ae33cb28007f6a9b48ac
Agustin Garbarino <agustin.garbarino@ascentis.com>
2020-03-06 14:34:21 +0000

    test

--- src/table.js
+++ src/table.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Table() {
-  return <div>This will be a table in the future</div>;
+function Table({ rows, title = 'Default title' }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div>Amount of rows: {rows.length}</div>
+    </div>
+  );
 }
+
+Table.propTypes = {
+  rows: PropTypes.array.isRequired,
+  title: PropTypes.string
+};
 
 export default Table;
