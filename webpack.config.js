13484dc3b26356c2109af4661d309bcdb117c442
Agustin Garbarino <agustin.garbarino@ascentis.com>
2020-03-06 14:24:42 +0000

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/table.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve('lib'),
     filename: 'index.bundle.js',
